20e994505af29d9552d52a13f532e4f191031f9f
Naokyoo <ben.choisyornia@gmail.com>
2026-01-15 23:34:41 +0100

    fix: update resourcesUrl in main.js to main/branch root

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ const CONFIG = {
   discordLink: 'https://discord.gg/68QG8PsgnA',
   websiteLink: 'https://cobblemon.fr', // À modifier
   modsUrl: 'https://raw.githubusercontent.com/Naokyoo/modlistcobblemonofthewild/main/mods.json',
-  resourcesUrl: 'https://raw.githubusercontent.com/Naokyoo/modlistcobblemonofthewild/master/github-mods/resources.json',
+  resourcesUrl: 'https://raw.githubusercontent.com/Naokyoo/modlistcobblemonofthewild/main/resources.json',
   updateUrl: 'https://github.com/Naokyoo/modlistcobblemonofthewild/releases/latest/download'
 };
 
